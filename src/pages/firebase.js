e217119d210f8769dd7209d2b4210608d15fd4a1
Sakshi Tiwari <96986663+sakshi8707@users.noreply.github.com>
2024-09-20 14:43:25 +0530

    Removed Anaylatics

--- src/pages/firebase.js
+++ src/pages/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getAnalytics } from "firebase/analytics";
+
 import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 
@@ -16,7 +16,7 @@ const firebaseConfig = {
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
 const auth = getAuth(app);
 const db = getFirestore(app);
 export { app, auth, db };
